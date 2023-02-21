7654213854d45315808601dd3de9b22d4b5dc1df
Fabien Roger <efab.roger@gmail.com>
2023-02-21 09:01:20 +0000

    Clean metrics

--- code/calMetric.py
+++ code/calMetric.py
@@ -26,297 +26,19 @@ import torchvision.transforms as transforms
 import numpy as np
 import time
 from scipy import misc
+from sklearn.metrics import roc_auc_score, roc_curve
 
 
-def to_np_float(f):
-    return np.float(f)  # type: ignore
-
-
-def tpr95(name):
+def tpr95(labels, data):
     # calculate the falsepositive error when tpr is 95%
-    # calculate baseline
-    T = 1
-    cifar = np.loadtxt("./softmax_scores/confidence_Base_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Base_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 1
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 1
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    total = 0.0
-    fpr = 0.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        error2 = np.sum(np.sum(Y1 > delta)) / to_np_float(len(Y1))
-        if tpr <= 0.9505 and tpr >= 0.9495:
-            fpr += error2
-            total += 1
-    fprBase = fpr / total
 
-    # calculate our algorithm
-    T = 1000
-    cifar = np.loadtxt("./softmax_scores/confidence_Our_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Our_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 0.12
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 0.0104
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    total = 0.0
-    fpr = 0.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        error2 = np.sum(np.sum(Y1 > delta)) / to_np_float(len(Y1))
-        if tpr <= 0.9505 and tpr >= 0.9495:
-            fpr += error2
-            total += 1
-    fprNew = fpr / total
-
-    return fprBase, fprNew
+    fpr, tpr, thresholds = roc_curve(labels, data)
+    tpr95_index = np.where(tpr >= 0.95)[0][0]
+    return fpr[tpr95_index]
 
 
-def auroc(name):
-    # calculate the AUROC
-    # calculate baseline
-    T = 1
-    cifar = np.loadtxt("./softmax_scores/confidence_Base_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Base_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 1
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 1
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    aurocBase = 0.0
-    fprTemp = 1.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        fpr = np.sum(np.sum(Y1 > delta)) / to_np_float(len(Y1))
-        aurocBase += (-fpr + fprTemp) * tpr
-        fprTemp = fpr
-    aurocBase += fpr * tpr  # type: ignore
-    # calculate our algorithm
-    T = 1000
-    cifar = np.loadtxt("./softmax_scores/confidence_Our_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Our_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 0.12
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 0.0104
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    aurocNew = 0.0
-    fprTemp = 1.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        fpr = np.sum(np.sum(Y1 >= delta)) / to_np_float(len(Y1))
-        aurocNew += (-fpr + fprTemp) * tpr
-        fprTemp = fpr
-    aurocNew += fpr * tpr  # type: ignore
-    return aurocBase, aurocNew
-
-
-def auprIn(name):
-    # calculate the AUPR
-    # calculate baseline
-    T = 1
-    cifar = np.loadtxt("./softmax_scores/confidence_Base_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Base_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 1
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 1
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    precisionVec = []
-    recallVec = []
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    auprBase = 0.0
-    recallTemp = 1.0
-    for delta in np.arange(start, end, gap):
-        tp = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        fp = np.sum(np.sum(Y1 >= delta)) / to_np_float(len(Y1))
-        if tp + fp == 0:
-            continue
-        precision = tp / (tp + fp)
-        recall = tp
-        precisionVec.append(precision)
-        recallVec.append(recall)
-        auprBase += (recallTemp - recall) * precision
-        recallTemp = recall
-    auprBase += recall * precision  # type: ignore
-    # print(recall, precision)
-
-    # calculate our algorithm
-    T = 1000
-    cifar = np.loadtxt("./softmax_scores/confidence_Our_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Our_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 0.12
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 0.0104
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    auprNew = 0.0
-    recallTemp = 1.0
-    for delta in np.arange(start, end, gap):
-        tp = np.sum(np.sum(X1 >= delta)) / to_np_float(len(X1))
-        fp = np.sum(np.sum(Y1 >= delta)) / to_np_float(len(Y1))
-        if tp + fp == 0:
-            continue
-        precision = tp / (tp + fp)
-        recall = tp
-        # precisionVec.append(precision)
-        # recallVec.append(recall)
-        auprNew += (recallTemp - recall) * precision
-        recallTemp = recall
-    auprNew += recall * precision  # type: ignore
-    return auprBase, auprNew
-
-
-def auprOut(name):
-    # calculate the AUPR
-    # calculate baseline
-    T = 1
-    cifar = np.loadtxt("./softmax_scores/confidence_Base_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Base_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 1
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 1
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    auprBase = 0.0
-    recallTemp = 1.0
-    for delta in np.arange(end, start, -gap):
-        fp = np.sum(np.sum(X1 < delta)) / to_np_float(len(X1))
-        tp = np.sum(np.sum(Y1 < delta)) / to_np_float(len(Y1))
-        if tp + fp == 0:
-            break
-        precision = tp / (tp + fp)
-        recall = tp
-        auprBase += (recallTemp - recall) * precision
-        recallTemp = recall
-    auprBase += recall * precision  # type: ignore
-
-    # calculate our algorithm
-    T = 1000
-    cifar = np.loadtxt("./softmax_scores/confidence_Our_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Our_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 0.12
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 0.0104
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    auprNew = 0.0
-    recallTemp = 1.0
-    for delta in np.arange(end, start, -gap):
-        fp = np.sum(np.sum(X1 < delta)) / to_np_float(len(X1))
-        tp = np.sum(np.sum(Y1 < delta)) / to_np_float(len(Y1))
-        if tp + fp == 0:
-            break
-        precision = tp / (tp + fp)
-        recall = tp
-        auprNew += (recallTemp - recall) * precision
-        recallTemp = recall
-    auprNew += recall * precision  # type: ignore
-    return auprBase, auprNew
-
-
-def detection(name):
-    # calculate the minimum detection error
-    # calculate baseline
-    T = 1
-    cifar = np.loadtxt("./softmax_scores/confidence_Base_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Base_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 1
-    elif name == "CIFAR-100":
-        start = 0.01
-        end = 1
-    else:
-        raise ValueError(f"Invalid dataset name {name}")
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    errorBase = 1.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 < delta)) / to_np_float(len(X1))
-        error2 = np.sum(np.sum(Y1 > delta)) / to_np_float(len(Y1))
-        errorBase = np.minimum(errorBase, (tpr + error2) / 2.0)
-
-    # calculate our algorithm
-    T = 1000
-    cifar = np.loadtxt("./softmax_scores/confidence_Our_In.txt", delimiter=",")
-    other = np.loadtxt("./softmax_scores/confidence_Our_Out.txt", delimiter=",")
-    if name == "CIFAR-10":
-        start = 0.1
-        end = 0.12
-    if name == "CIFAR-100":
-        start = 0.01
-        end = 0.0104
-    gap = (end - start) / 100000
-    # f = open("./{}/{}/T_{}.txt".format(nnName, dataName, T), 'w')
-    Y1 = other[:, 2]
-    X1 = cifar[:, 2]
-    errorNew = 1.0
-    for delta in np.arange(start, end, gap):
-        tpr = np.sum(np.sum(X1 < delta)) / to_np_float(len(X1))
-        error2 = np.sum(np.sum(Y1 > delta)) / to_np_float(len(Y1))
-        errorNew = np.minimum(errorNew, (tpr + error2) / 2.0)
-
-    return errorBase, errorNew
+def auroc(labels, data):
+    return roc_auc_score(labels, data)
 
 
 def metric(nn, data):
@@ -349,18 +71,25 @@ def metric(nn, data):
         dataName = "Uniform Noise"
     else:
         raise ValueError(f"Invalid dataset name {data}")
-    fprBase, fprNew = tpr95(indis)
-    errorBase, errorNew = detection(indis)
-    aurocBase, aurocNew = auroc(indis)
-    auprinBase, auprinNew = auprIn(indis)
-    auproutBase, auproutNew = auprOut(indis)
+
+    labels_and_data = []
+    for algorithm in ["Base", "Our"]:
+        datas = [
+            np.loadtxt(f"./softmax_scores/confidence_{algorithm}_{part}.txt", delimiter=",")[:, 2]
+            for part in ["In", "Out"]
+        ]
+        assert datas[0].shape == datas[1].shape
+        data = np.concatenate(datas)
+        labels = np.concatenate([np.ones(len(data) // 2), np.zeros(len(data) // 2)])
+        labels_and_data.append((labels, data))
+
+    base, our = labels_and_data
+    fprBase, fprNew = tpr95(*base), tpr95(*our)
+    aurocBase, aurocNew = auroc(*base), auroc(*our)
     print("{:31}{:>22}".format("Neural network architecture:", nnStructure))
     print("{:31}{:>22}".format("In-distribution dataset:", indis))
     print("{:31}{:>22}".format("Out-of-distribution dataset:", dataName))
     print("")
     print("{:>34}{:>19}".format("Baseline", "Our Method"))
     print("{:20}{:13.1f}%{:>18.1f}% ".format("FPR at TPR 95%:", fprBase * 100, fprNew * 100))
-    print("{:20}{:13.1f}%{:>18.1f}%".format("Detection error:", errorBase * 100, errorNew * 100))
     print("{:20}{:13.1f}%{:>18.1f}%".format("AUROC:", aurocBase * 100, aurocNew * 100))
-    print("{:20}{:13.1f}%{:>18.1f}%".format("AUPR In:", auprinBase * 100, auprinNew * 100))
-    print("{:20}{:13.1f}%{:>18.1f}%".format("AUPR Out:", auproutBase * 100, auproutNew * 100))
